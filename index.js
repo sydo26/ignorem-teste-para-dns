9ff20a20b2c461521120697b238a307e84c292f3
sydo26 <sydoafk@gmail.com>
2021-04-29 13:11:07 -0300

    bla

--- index.js
+++ index.js
@@ -7,10 +7,28 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
+app.get('/*',(req, res) => {
 
-    const { hostname, params, httpVersion, headers, baseUrl, path, cookies, httpVersionMajor, httpVersionMinor, method, originalUrl, subdomains, url, protocol } = req
+    const {
+        hostname,
+        params,
+        httpVersion,
+        headers,
+        baseUrl,
+        path,
+        cookies,
+        httpVersionMajor,
+        httpVersionMinor,
+        method,
+        originalUrl,
+        subdomains,
+        url,
+        protocol,
+        socket: { remoteAddress }
+    } = req
+    
     return res.status(200).json({
+        remoteAddress,
         subdomains,
         protocol,
         url,
